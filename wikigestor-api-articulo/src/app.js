21cadabddc1a527edfe0157f085166667882154d
julianmerinodev <julianmerino720@gmail.com>
2025-08-04 13:00:09 -0600

    Configuración de middleware para validar jwt

--- wikigestor-api-articulo/src/app.js
+++ wikigestor-api-articulo/src/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const articuloRoutes = require('./routes/articulo.routes');
 const setupSwagger = require('./docs/swagger');
+const verificarToken = require('./middlewares/verificarToken');
 
 const app = express();
 
 app.use(express.json());
-app.use('/api/articulos', articuloRoutes);
+
 //Swagger
 setupSwagger(app);
 
+//Middleware para verificar token
+app.use(verificarToken);
+
+//Rutas de endpoints
+app.use('/api/articulos', articuloRoutes);
 
 module.exports = app;
